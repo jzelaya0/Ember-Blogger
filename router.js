2cbefe1754cbeb0ed833992737b3fc50967fbf9c
Jesse Zelaya <jessezelaya1@gmail.com>
2015-11-18 19:32:09 -0800

    Created email and phone as nested routes in contact

--- router.js
+++ router.js
@@ -3,5 +3,9 @@ Blogger.Router.map(function(){
   //This is where routes are set
   this.resource('posts', {path: '/'});//Set as homepage(overrides default)
   this.resource('about');
-  this.resource('contact')
+  this.resource('contact',function() {//Nested Route
+    this.resource('phone');
+    this.resource('email');
+  });
+
 })
