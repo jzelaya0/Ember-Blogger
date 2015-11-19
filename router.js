a0fceb3295936e0a9479b03f60d13414f71966fb
Jesse Zelaya <jessezelaya1@gmail.com>
2015-11-18 19:16:11 -0800

    Set posts page as default route

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 //Blogger refers to app.js
 Blogger.Router.map(function(){
   //This is where routes are set
-  this.resource('posts');
+  this.resource('posts', {path: '/'});//Set as homepage(overrides default)
   this.resource('about');
   this.resource('contact')
 })
