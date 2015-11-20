80fe3c076e91c9e28b27fc2a183bb294d3378cb3
Jesse Zelaya <jessezelaya1@gmail.com>
2015-11-19 20:33:11 -0800

    Created action to show image

--- controllers/about.js
+++ controllers/about.js
@@ -1,7 +1,12 @@
 Blogger.AboutController = Ember.Controller.extend({
+  isPictureShowing: false, // to use in template
   actions: {
     showRealName: function(){
       alert('Dracula is fictional, but was inspired by the 15th century Wallachian Prince Vlad III the Impaler');
+    },
+    showPicture: function(){
+      //Ember depends on .set() to obeserve changes in objects properties
+      this.set('isPictureShowing', true);
     }
   }
-})
+});
