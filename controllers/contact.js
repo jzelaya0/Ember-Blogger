425a7e3c12625021585e8c034f0b1b5bd4d914cc
Jesse Zelaya <jessezelaya1@gmail.com>
2015-11-19 20:42:34 -0800

    Added feature to give users confirmation number when message is sent

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,7 +1,10 @@
 Blogger.ContactController = Ember.Controller.extend({
+  messageSent: false,
   actions:{
     sendMessage : function(){
       var message = prompt('Type message here:');
+      this.set('confirmationNumber', Math.round(Math.random() * 10000));
+      this.set('messageSent', true);
     }
   }
 });
